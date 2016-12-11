56400208fd2b10c6234259f34ebccec45dd21491
Paweł Adamski <pawel@agflow.com>
2016-12-11 21:39:06 +0100

    more tests, introduce gocheck

--- algorithm_test.go
+++ algorithm_test.go
@@ -3,9 +3,17 @@ package main
 import (
 	"encoding/json"
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
-func TestParsing(t *testing.T) {
+func Test(t *testing.T) { TestingT(t) }
+
+type ParsingSuite struct{}
+
+var _ = Suite(&ParsingSuite{})
+
+func (s *ParsingSuite) TestParsing(c *C) {
 	input := `{
     "RoundNumber": 3,
     "BotId": "a88454b0‐80ba‐4c10‐b162‐f1ca766f1e3f",
@@ -57,8 +65,17 @@ func TestParsing(t *testing.T) {
 }`
 	state := GameState{}
 	err := json.Unmarshal([]byte(input), &state)
-	if err != nil {
-		t.Fatal(err)
+	c.Assert(err, IsNil)
+	c.Assert(state.BotLocation, Equals, Location{x: 0, y: 0})
+	expectedBomb := Bomb{
+		Location:            Location{x: 0, y: 1},
+		ExplosionRadius:     2,
+		RoundsUntilExplodes: 3,
 	}
-
+	c.Assert(state.Bombs, DeepEquals, []Bomb{expectedBomb})
+	col1 := []int{2, 2, 3}
+	col2 := []int{0, 0, 1}
+	col3 := []int{0, 0, 0}
+	expectedBoard := [][]int{col1, col2, col3}
+	c.Assert(state.Board, DeepEquals, expectedBoard)
 }
