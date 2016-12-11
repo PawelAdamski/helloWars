0afe584abcc55c6fa6b07a8106ec12cd4b22f88a
Paweł Adamski <pawel@agflow.com>
2016-12-11 21:45:12 +0100

    deathMap

---
+++ deathMap.go
@@ -0,0 +1,9 @@
+package main
+
+// DeathMap hold information if according to current game state there will explosion on each field. If yes how many round from now.
+type DeathMap [][]int
+
+func mkDeathMap(board Board, bombs []Bomb, missiles []Missile) DeathMap {
+
+	return DeathMap{}
+}
